8b907d118b5c0975fe5f88f52d25d2fcb4678ce1
Petar Georgiev <petiyo07@abv.bg>
2023-08-24 00:04:02 +0300

    some changes made

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 const http = require("http");
 const fs = require("fs");
+const db = require("./db/db");
+require("dotenv").config();
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        email TEXT NOT NULL,
+        password TEXT NOT NULL
+    )
+    `);
+  console.log("Table created");
+});
+db.close();
+
 const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    fs.createReadStream("public/index.html").pipe(res);
+  if (req.url === "/login") {
+    fs.createReadStream("public/login.html").pipe(res);
+  } else if (req.method === "POST" && req.url === "/login/register") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+      console.log(chunk.toString());
+    });
+    req.on("end", () => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ message: `${body}` }));
+    });
   }
 });
 
