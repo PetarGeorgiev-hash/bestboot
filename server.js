fdb87561b5ae3298c344d75b48cfaf2413111af8
Petar Georgiev <petiyo07@abv.bg>
2023-08-23 22:52:09 +0300

    FIrst test start

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const http = require("http");
 const fs = require("fs");
+const PORT = process.env.PORT || 3000;
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.createReadStream("public/index.html").pipe(res);
+  }
+});
 
-const server = http.createServer((res, req) => {});
+server.listen(PORT, () => {
+  console.log("The server is on " + PORT);
+});
